510bacb34b8e4948e38df4227bd7a99e0f4be223
ericpandrade <ericpandrade085@gmail.com>
2021-08-27 18:18:37 -0300

    Começando a usar a API

---
+++ script.js
@@ -0,0 +1,48 @@
+// Global
+const people = document.querySelector("#characters");
+const moons = document.querySelector("#moons");
+const planets = document.querySelector("#planets");
+const spaceships = document.querySelector("#spaceships");
+
+//  Getting the api data needed for the card
+
+getData = async (params) => {
+  const getData = await fetch(`http://swapi.dev/api/${params}/`);
+  const data = await getData.json();
+  return data;
+};
+
+// Card values are shown at a small time interval
+
+fillCards = async () => {
+  const countPeoples = await getData("people");
+  people.innerHTML = `${countPeoples.count}`;
+
+  const countMoons = await getData("vehicles");
+  moons.innerHTML = `${countMoons.count}`;
+
+  const countPlanets = await getData("planets");
+  planets.innerHTML = `${countPlanets.count}`;
+
+  const countStarchips = await getData("starships");
+  spaceships.innerHTML = `${countStarchips.count}`;
+};
+
+/* 
+    //  Card values are shown at the same time.
+    fillCards = () => {
+    Promise.all([
+        getData("people"),
+        getData("vehicles"),
+        getData("planets"),
+        getData("starships"),
+    ]).then((counts) => {
+        people.innerHTML = `${counts[0]}`;
+        moons.innerHTML = `${counts[1]}`;
+        planets.innerHTML = `${counts[2]}`;
+        spaceships.innerHTML = `${counts[3]}`;
+    });
+    }; 
+*/
+
+fillCards();
